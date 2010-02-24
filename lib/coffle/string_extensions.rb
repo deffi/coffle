50fc2ea032aed4abb14b9c42a6fb71bdd80f2a35
deffi <deffi@c28bc943-ea5a-4777-b63e-cba72211b6f8>
2010-02-24 21:25:57 +0000

    Added ERB processing to Builder Introduced TemplateMethods Added host command to Builder Added automessage to Builder Added unit tests for string extensions (wrap and prefix_lines)

---
+++ lib/coffle/string_extensions.rb
@@ -0,0 +1,11 @@
+class String
+	def wrap(width)
+		# Improved regexp: may also break after a slash (not replacing it)
+		gsub(/(.{1,#{width}})( +|$\n?|(\/))|(.{1,#{width}})/, "\\1\\3\\4\n") 
+	end
+
+	def prefix_lines(prefix)
+		split("\n").map { |line| prefix+line }.join("\n")
+	end
+end
+
--- lib/coffle/template_methods.rb
+++ lib/coffle/template_methods.rb
@@ -1,4 +1,3 @@
-require 'erb'
 require 'socket'
 
 module Coffle
@@ -33,35 +32,4 @@ end
 	end
 end
 
-module Coffle
-	class Builder
-		# Include modules that should be available to the templates in
-		# TemplateMethods
-		include TemplateMethods
-
-		def Builder.build(source, target)
-			#FileUtils.copy_file source, target, preserve=false, dereference=true
-			Builder.new.build source,target
-		end
-
-		def initialize
-		end
-
-		def process(text)
-			template=ERB.new(text, nil, "-")
-			template.result(binding)
-		end
-
-		def build(source, target)
-			#FileUtils.copy_file @source, @target, preserve=false, dereference=true
-
-			@source=source
-			@target=target
-
-			File.open(@target, "w") do |file|
-				file.print process(File.read(@source))
-			end
-		end
-	end
-end
 

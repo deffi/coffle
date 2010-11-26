c7c3aa8727f7bd810f110668c049a0691ae06928
deffi <deffi@c28bc943-ea5a-4777-b63e-cba72211b6f8>
2010-11-26 21:05:22 +0000

    Added fill_length and format_table array helpers

---
+++ lib/coffle/array_extensions.rb
@@ -0,0 +1,33 @@
+class Array
+	def fill_length!(target_length, value=nil)
+		return if target_length<=self.length
+
+		self.concat [value]*(target_length-self.length)
+	end
+
+	def fill_length(target_length, value=nil)
+		result=self.dup
+		result.fill_length!(target_length, value)
+		result
+	end
+
+	def format_table(separator=" ")
+		# Determine the maximum size of each column
+		column_widths=[]
+
+		self.each do |row|
+			column_widths.fill_length!(row.length, 0)
+
+			row.each_with_index do |value, column|
+				column_widths[column]=[column_widths[column], value.to_s.length].max
+			end
+		end
+
+		self.map { |row|
+			row.each_with_index.map { |value, column|
+				value.to_s.ljust(column_widths[column])
+			}.join(separator)
+		}.join("\n")
+	end
+end
+

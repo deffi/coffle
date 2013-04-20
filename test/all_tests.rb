fb8ed1909a4c9c5e0a2db1035d5240a88a1b5776
Martin Herrmann <martin@famherrmann.de>
2013-04-20 21:57:35 +0200

    Fix test paths

--- test/all_tests.rb
+++ test/all_tests.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
+$: << File.dirname(__FILE__)
+
 Dir.glob("#{File.dirname(__FILE__)}/*_test.rb").each do |entry|
-	require "#{entry}"
+	require "#{File.basename(entry)}"
 end
 
 # at_axit, the AutoRunner will be invoked
